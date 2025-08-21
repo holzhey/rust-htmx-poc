4828c3728aeb4c607e3396c4da3a067feb9fe8b6
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2025-08-21 20:36:28 +0200

    Add index html

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use axum::{Router, routing::get};
-use maud::{Markup, html};
+use maud::{DOCTYPE, Markup, html};
 
 #[tokio::main]
 async fn main() {
@@ -10,6 +10,24 @@ async fn main() {
 
 async fn root() -> Markup {
     html! {
-        h1 { "Hello" }
+        (DOCTYPE)
+        html lang="en" {
+            head {
+                link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" {}
+                script src="https://unpkg.com/htmx.org@2.0.1" {}
+            }
+            body {
+                main class="container" {
+                    section {
+                        div id="parent-div" {}
+                        button hx-post="/clicked" hx-trigger="click" hx-target="#parent-div" hx-swap="outerHTML" { "Click Me!" }
+                    }
+                    section {
+                        input type="text" name="q" hx-get="/search" hx-trigger="keyup changed delay:500ms" hx-target="#search-results" placeholder="Search..." {}
+                        div { "Results go here" }
+                    }
+                }
+            }
+        }
     }
 }
