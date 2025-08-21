195760455393bc2a33f725b15031231519c92fa0
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2025-08-21 22:37:07 +0200

    Add search feature

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use axum::{Router, routing::get};
+use axum::{Router, extract::Query, routing::get};
 use maud::{DOCTYPE, Markup, html};
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(index));
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/search", get(search));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
@@ -33,10 +36,42 @@ async fn index() -> Markup {
                               hx-trigger="keyup changed delay:500ms"
                               hx-target="#search-results"
                               placeholder="Search..." {}
-                        div { "Results go here" }
+                        (results(get_results()))
                     }
                 }
             }
         }
     }
+}
+
+#[derive(Deserialize)]
+struct SearchRequest {
+    q: String,
+}
+
+async fn search(q: Query<SearchRequest>) -> Markup {
+    results(get_query_results(&q.q))
+}
+
+fn results(results: Vec<&'static str>) -> Markup {
+    html! {
+        div id="search-results" {
+            ul {
+                @for result in results {
+                    li { (result) }
+                }
+            }
+        }
+    }
+}
+
+fn get_results() -> Vec<&'static str> {
+    vec!["one", "two", "three", "four"]
+}
+
+fn get_query_results(query: &str) -> Vec<&'static str> {
+    get_results()
+        .into_iter()
+        .filter(|v| v.starts_with(query))
+        .collect()
 }
