7c2c2985db2499614d3efe5b3793d275ff392596
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2025-08-21 20:40:13 +0200

    Improve readability

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,36 @@ use maud::{DOCTYPE, Markup, html};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().route("/", get(index));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> Markup {
+async fn index() -> Markup {
     html! {
         (DOCTYPE)
         html lang="en" {
             head {
-                link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" {}
+                link rel="stylesheet"
+                     href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" {}
                 script src="https://unpkg.com/htmx.org@2.0.1" {}
             }
             body {
                 main class="container" {
                     section {
                         div id="parent-div" {}
-                        button hx-post="/clicked" hx-trigger="click" hx-target="#parent-div" hx-swap="outerHTML" { "Click Me!" }
+                        button hx-post="/clicked"
+                               hx-trigger="click"
+                               hx-target="#parent-div"
+                               hx-swap="outerHTML" { "Click Me!" }
                     }
                     section {
-                        input type="text" name="q" hx-get="/search" hx-trigger="keyup changed delay:500ms" hx-target="#search-results" placeholder="Search..." {}
+                        input type="text"
+                              name="q"
+                              hx-get="/search"
+                              hx-trigger="keyup changed delay:500ms"
+                              hx-target="#search-results"
+                              placeholder="Search..." {}
                         div { "Results go here" }
                     }
                 }
